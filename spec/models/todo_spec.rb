21891b33873130403002cdf8f236f539b9e90f10
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2014-11-19 21:33:32 -0800

    create failing tests for todo

---
+++ spec/models/todo_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Todo, 'validations' do
+  it { expect(subject).to validate_presence_of(:title) }
+  it { expect(subject).to validate_presence_of(:description) }
+  it { expect(subject).to validate_presence_of(:urgency) }
+end
